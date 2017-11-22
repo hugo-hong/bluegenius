2d64c3b68198a1ce14fc44126c148d76bd1787f7
hugo-hong <hugo.08@163.com>
2017-11-22 11:15:22 +0800

    Update thread.cxx

--- utils/src/thread.cxx
+++ utils/src/thread.cxx
@@ -1,4 +1,4 @@
-/*
+/*********************************************************************************
    Bluegenius - Bluetooth host protocol stack for Linux/android/windows...
    Copyright (C) 
    Written 2017 by hugo（yongguang hong） <hugo.08@163.com>
@@ -16,7 +16,7 @@
    ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS,
    COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
    SOFTWARE IS DISCLAIMED.
-*/ 
+*********************************************************************************/ 
 
 #define LOG_TAG "thread_proxy"
 
@@ -27,6 +27,8 @@
 
 #include"thread.h"
 
+static const size_t DEFAULT_WORK_QUEUE_CAPACITY = 128;
+
 struct thread_t {
   std::atomic_bool is_joined{false};
   pthread_t pthread;
@@ -36,9 +38,9 @@ struct thread_t {
   fixed_queue_t* work_queue;
 };
 
-struct start_arg {
+struct entry_arg {
   thread_t* thread;
-  semaphore_t* start_sem;
+  event_t* entry_evt;
   int error;
 };
 
@@ -48,4 +50,47 @@ typedef struct {
 } work_item_t;
 
 thread_t* thread_new(const char* name) {
+   CHECK(name != NULL);
+   
+   thread_t* thread = static_cast<thread_t*>(sys_calloc(sizeof(thread_t)));
+   thread->reactor = reactor_new();
+   if (NULL == thread->reactor) goto error;
+   
+   thread->work_queue = fixed_queue_new(DEFAULT_WORK_QUEUE_CAPACITY);
+   if (NULL == thread->work_queue) goto error;
+   
+   // Start is on the stack, but we use a semaphore, so it's safe
+  struct entry_arg arg;
+  arg.entry_evt = event_new(0);
+  if (NULL == arg.entry_evt) goto error;
+
+  strncpy(thread->name, name, THREAD_NAME_MAX);
+  arg.thread = thread;
+  arg.error = 0;
+  pthread_create(&ret->pthread, NULL, run_thread, &arg);
+  event_wait(arg.entry_evt);
+  event_free(arg.entry_evt);
+
+  if (entry.error) goto error;
+
+  return thread;
+
+error:
+  if (thread) {
+    fixed_queue_free(ret->work_queue, osi_free);
+    reactor_free(ret->reactor);
+  }
+  sys_free(thread);
+  return NULL;
+}
+
+void thread_free(thread_t* thread) {
+  if (!thread) return;
+
+  thread_stop(thread);
+  thread_join(thread);
+
+  fixed_queue_free(thread->work_queue, sys_free);
+  reactor_free(thread->reactor);
+  sys_free(thread);
 }
