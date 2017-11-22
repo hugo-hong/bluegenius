c856209a671012b82741703fca954a4eaba2c019
hugo-hong <hugo.08@163.com>
2017-11-22 12:21:25 +0800

    Update thread.cxx

--- utils/src/thread.cxx
+++ utils/src/thread.cxx
@@ -94,3 +94,130 @@ void thread_free(thread_t* thread) {
   reactor_free(thread->reactor);
   sys_free(thread);
 }
+
+void thread_join(thread_t* thread) {
+  CHECK(thread != NULL);
+
+  if (!std::atomic_exchange(&thread->is_joined, true))
+    pthread_join(thread->pthread, NULL);
+}
+
+bool thread_post(thread_t* thread, thread_fn func, void* context) {
+  CHECK(thread != NULL);
+  CHECK(func != NULL);
+
+  // TODO(sharvil): if the current thread == |thread| and we've run out
+  // of queue space, we should abort this operation, otherwise we'll
+  // deadlock.
+
+  // Queue item is freed either when the queue itself is destroyed
+  // or when the item is removed from the queue for dispatch.
+  work_item_t* item = (work_item_t*)sys_malloc(sizeof(work_item_t));
+  item->func = func;
+  item->context = context;
+  fixed_queue_enqueue(thread->work_queue, item);
+  return true;
+}
+
+void thread_stop(thread_t* thread) {
+  CHECK(thread != NULL);
+  reactor_stop(thread->reactor);
+}
+
+bool thread_set_priority(thread_t* thread, int priority) {
+  if (!thread) return false;
+
+  const int rc = setpriority(PRIO_PROCESS, thread->tid, priority);
+  if (rc < 0) {
+    LOG_ERROR(LOG_TAG,
+              "%s unable to set thread priority %d for tid %d, error %d",
+              __func__, priority, thread->tid, rc);
+    return false;
+  }
+
+  return true;
+}
+
+bool thread_set_rt_priority(thread_t* thread, int priority) {
+  if (!thread) return false;
+
+  struct sched_param rt_params;
+  rt_params.sched_priority = priority;
+
+  const int rc = sched_setscheduler(thread->tid, SCHED_FIFO, &rt_params);
+  if (rc != 0) {
+    LOG_ERROR(LOG_TAG,
+              "%s unable to set SCHED_FIFO priority %d for tid %d, error %s",
+              __func__, priority, thread->tid, strerror(errno));
+    return false;
+  }
+
+  return true;
+}
+
+bool thread_is_self(const thread_t* thread) {
+  CHECK(thread != NULL);
+  return !!pthread_equal(pthread_self(), thread->pthread);
+}
+
+reactor_t* thread_get_reactor(const thread_t* thread) {
+  CHECK(thread != NULL);
+  return thread->reactor;
+}
+
+const char* thread_name(const thread_t* thread) {
+  CHECK(thread != NULL);
+  return thread->name;
+}
+
+static void* run_thread(void* arg) {
+  CHECK(arg != NULL);
+
+  struct entry_arg* entry = static_cast<struct entry_arg*>(arg);
+  thread_t* thread = entry->thread;
+
+  CHECK(thread != NULL);
+
+  if (prctl(PR_SET_NAME, (unsigned long)thread->name) == -1) {
+    LOG_ERROR(LOG_TAG, "%s unable to set thread name: %s", __func__,
+              strerror(errno));
+    entry->error = errno;
+    event_post(entry->entry_evt);
+    return NULL;
+  }
+  thread->tid = gettid();
+
+  LOG_INFO(LOG_TAG, "%s: thread id %d, thread name %s started", __func__,
+           thread->tid, thread->name);
+
+  event_post(entry->entry_evt);
+
+  int fd = fixed_queue_get_dequeue_fd(thread->work_queue);
+  void* context = thread->work_queue;
+
+  reactor_object_t* work_queue_object =
+      reactor_register(thread->reactor, fd, context, work_queue_read_cb, NULL);
+  reactor_start(thread->reactor);
+  reactor_unregister(work_queue_object);
+
+  // Make sure we dispatch all queued work items before exiting the thread.
+  // This allows a caller to safely tear down by enqueuing a teardown
+  // work item and then joining the thread.
+  size_t count = 0;
+  work_item_t* item =
+      static_cast<work_item_t*>(fixed_queue_try_dequeue(thread->work_queue));
+  while (item && count <= fixed_queue_capacity(thread->work_queue)) {
+    item->func(item->context);
+    osi_free(item);
+    item =
+        static_cast<work_item_t*>(fixed_queue_try_dequeue(thread->work_queue));
+    ++count;
+  }
+
+  if (count > fixed_queue_capacity(thread->work_queue))
+    LOG_DEBUG(LOG_TAG, "%s growing event queue on shutdown.", __func__);
+
+  LOG_WARN(LOG_TAG, "%s: thread id %d, thread name %s exited", __func__,
+           thread->tid, thread->name);
+  return NULL;
+}
