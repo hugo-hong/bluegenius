53fab598d27a6abc690cce70fbbbe2800b7116e6
hugo-hong <hugo.08@163.com>
2017-11-27 16:54:02 +0800

    Create hci_layer.cxx

---
+++ hci/src/hci_layer.cxx
@@ -0,0 +1,40 @@
+/*********************************************************************************
+   Bluegenius - Bluetooth host protocol stack for Linux/android/windows...
+   Copyright (C) 
+   Written 2017 by hugo（yongguang hong） <hugo.08@163.com>
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as
+   published by the Free Software Foundation;
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS.
+   IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) AND AUTHOR(S) BE LIABLE FOR ANY
+   CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+   ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS,
+   COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
+   SOFTWARE IS DISCLAIMED.
+*********************************************************************************/
+#define LOG_TAG "bt_hci"
+
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// Abort if there is no response to an HCI command.
+static const uint32_t COMMAND_PENDING_TIMEOUT_MS = 2000;
+static const uint32_t COMMAND_TIMEOUT_RESTART_US = 500000;
+
+
+
+static future_t* start_up(void) {
+   LOG_INFO(LOG_TAG, "%s", __func__);
+
+}
+
+static future_t* shut_down() {
+}
+
