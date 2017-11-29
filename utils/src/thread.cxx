ad4f9c6e810d036e010d9f6b2726d0bd8f2231e6
hugo-hong <hugo.08@163.com>
2017-11-29 17:58:29 +0800

    Update thread.cxx

--- utils/src/thread.cxx
+++ utils/src/thread.cxx
@@ -27,206 +27,188 @@
 
 #include"thread.h"
 
-static const size_t DEFAULT_WORK_QUEUE_CAPACITY = 128;
-
-struct thread_t {
-  std::atomic_bool is_joined{false};
-  pthread_t pthread;
-  pid_t tid;
-  char name[THREAD_NAME_MAX + 1];
-  reactor_t* reactor;
-  fixed_queue_t* work_queue;
-};
-
-struct entry_arg {
-  thread_t* thread;
-  event_t* entry_evt;
+typedef struct {
+  Thread* thread;
+  Event* entry_evt;
   int error;
-};
+}entry_arg;
 
 typedef struct {
   thread_fn func;
   void* context;
 } work_item_t;
 
-thread_t* thread_new(const char* name) {
-   CHECK(name != NULL);
-   
-   thread_t* thread = static_cast<thread_t*>(sys_calloc(sizeof(thread_t)));
-   thread->reactor = reactor_new();
-   if (NULL == thread->reactor) goto error;
-   
-   thread->work_queue = fixed_queue_new(DEFAULT_WORK_QUEUE_CAPACITY);
-   if (NULL == thread->work_queue) goto error;
-   
-   // Start is on the stack, but we use a semaphore, so it's safe
-  struct entry_arg arg;
-  arg.entry_evt = event_new(0);
-  if (NULL == arg.entry_evt) goto error;
+Thread::Thread(const char* name, size_t size)
+    :m_isJoined(false)
+    ,m_pthread(NULL)
+    ,m_tid(-1)
+    ,m_pReactor(NULL)
+    ,m_pWorkQueue(NULL)
+{       
+    New(name, size);
+}
 
-  strncpy(thread->name, name, THREAD_NAME_MAX);
-  arg.thread = thread;
-  arg.error = 0;
-  pthread_create(&ret->pthread, NULL, run_thread, &arg);
-  event_wait(arg.entry_evt);
-  event_free(arg.entry_evt);
+Thread::~Thread() {
+    Free();
+}
 
-  if (entry.error) goto error;
+void Thread::Post(thread_fn func, void* context) {
+    CHECK(func != NULL);
+    CHECK(m_pWorkQueue != NULL);
+    
+    // TODO(sharvil): if the current thread == |thread| and we've run out
+    // of queue space, we should abort this operation, otherwise we'll
+    // deadlock.
 
-  return thread;
+    // Queue item is freed either when the queue itself is destroyed
+    // or when the item is removed from the queue for dispatch.
+    work_item_t* item = (work_item_t*)sys_malloc(sizeof(work_item_t));
+    item->func = func;
+    item->context = context;
+    m_pWorkQueue->Enqueue(item); 
+}
 
+void Thread::Stop() {
+    //stop reactor
+    if (m_pReactor != NULL) m_pReactor->Stop();
+}
+
+void Thread::Join() {
+    if (!std::atomic_exchange(&m_isJoined, true))
+        pthread_join(m_pthread, NULL);
+}
+
+void Thread::New(const char* name, size_t size) {
+    CHECK(name != NULL);
+    memset(m_name, 0, sizeof(m_name));
+    strncpy(m_name, name, THREAD_NAME_MAX);
+    
+    m_pReactor = new Reactor();
+    if (NULL == m_pReactor) goto error;
+    
+    m_pWorkQueue = new FixedQueue(size);
+    if (NULL == m_pWorkQueue) goto error;
+    
+    // Start is on the stack, but we use a event, so it's safe
+    entry_arg arg;
+    arg.thread = this;
+    arg.entry_evt = new Event(0);
+    arg.error = 0;
+    
+    pthread_create(&m_pthread, NULL, Thread::run_thread, &arg);
+    
+    arg.entry_evt->Wait();
+    delete arg.entry_evt;
+    if (arg.error) goto error;
+    
+    return;
+    
 error:
-  if (thread) {
-    fixed_queue_free(ret->work_queue, osi_free);
-    reactor_free(ret->reactor);
-  }
-  sys_free(thread);
-  return NULL;
+    free();
 }
 
-void thread_free(thread_t* thread) {
-  if (!thread) return;
-
-  thread_stop(thread);
-  thread_join(thread);
-
-  fixed_queue_free(thread->work_queue, sys_free);
-  reactor_free(thread->reactor);
-  sys_free(thread);
+void Thread::Free() {
+    Stop();
+    Join();
+    if (m_pReactor) delete m_pReactor;
+    if (m_pWorkQueue) delete m_pWorkQueue;
 }
 
-void thread_join(thread_t* thread) {
-  CHECK(thread != NULL);
+bool Thread::SetPriority(int priority) {
+    if (-1 == m_tid) return false;
 
-  if (!std::atomic_exchange(&thread->is_joined, true))
-    pthread_join(thread->pthread, NULL);
+    const int rc = setpriority(PRIO_PROCESS, m_tid, priority);
+    if (rc < 0) {
+        LOG_ERROR(LOG_TAG,
+            "%s unable to set thread priority %d for tid %d, error %d",
+            __func__, priority, m_tid, rc);
+        return false;
+    }
+
+    return true;
 }
 
-bool thread_post(thread_t* thread, thread_fn func, void* context) {
-  CHECK(thread != NULL);
-  CHECK(func != NULL);
+bool Thread::SetRTPriority(int priority) {
+    if (-1 == m_tid) return false;
 
-  // TODO(sharvil): if the current thread == |thread| and we've run out
-  // of queue space, we should abort this operation, otherwise we'll
-  // deadlock.
+    struct sched_param rt_params;
+    rt_params.sched_priority = priority;
 
-  // Queue item is freed either when the queue itself is destroyed
-  // or when the item is removed from the queue for dispatch.
-  work_item_t* item = (work_item_t*)sys_malloc(sizeof(work_item_t));
-  item->func = func;
-  item->context = context;
-  fixed_queue_enqueue(thread->work_queue, item);
-  return true;
-}
-
-void thread_stop(thread_t* thread) {
-  CHECK(thread != NULL);
-  reactor_stop(thread->reactor);
-}
-
-bool thread_set_priority(thread_t* thread, int priority) {
-  if (!thread) return false;
-
-  const int rc = setpriority(PRIO_PROCESS, thread->tid, priority);
-  if (rc < 0) {
-    LOG_ERROR(LOG_TAG,
-              "%s unable to set thread priority %d for tid %d, error %d",
-              __func__, priority, thread->tid, rc);
-    return false;
-  }
-
-  return true;
-}
-
-bool thread_set_rt_priority(thread_t* thread, int priority) {
-  if (!thread) return false;
-
-  struct sched_param rt_params;
-  rt_params.sched_priority = priority;
-
-  const int rc = sched_setscheduler(thread->tid, SCHED_FIFO, &rt_params);
-  if (rc != 0) {
-    LOG_ERROR(LOG_TAG,
+    const int rc = sched_setscheduler(m_tid, SCHED_FIFO, &rt_params);
+    if (rc != 0) {
+        LOG_ERROR(LOG_TAG,
               "%s unable to set SCHED_FIFO priority %d for tid %d, error %s",
-              __func__, priority, thread->tid, strerror(errno));
-    return false;
-  }
+              __func__, priority, m_tid, strerror(errno));
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
-bool thread_is_self(const thread_t* thread) {
-  CHECK(thread != NULL);
-  return !!pthread_equal(pthread_self(), thread->pthread);
+bool Thread::IsSelf() {
+    CHECK(m_pthread != NULL);
+    return !!pthread_equal(pthread_self(), m_pthread);
 }
 
-reactor_t* thread_get_reactor(const thread_t* thread) {
-  CHECK(thread != NULL);
-  return thread->reactor;
-}
+void* Thread::Run(void* arg) {
+    struct entry_arg* entry = static_cast<entry_arg*>(arg);    
 
-const char* thread_name(const thread_t* thread) {
-  CHECK(thread != NULL);
-  return thread->name;
-}
-
-static void* run_thread(void* arg) {
-  CHECK(arg != NULL);
-
-  struct entry_arg* entry = static_cast<struct entry_arg*>(arg);
-  thread_t* thread = entry->thread;
-
-  CHECK(thread != NULL);
-
-  if (prctl(PR_SET_NAME, (unsigned long)thread->name) == -1) {
-    LOG_ERROR(LOG_TAG, "%s unable to set thread name: %s", __func__,
+    if (prctl(PR_SET_NAME, (unsigned long)m_name) == -1) {
+        LOG_ERROR(LOG_TAG, "%s unable to set thread name: %s", __func__,
               strerror(errno));
-    entry->error = errno;
-    event_post(entry->entry_evt);
+        entry->error = errno;
+        entry->entry_evt->Post();
+        return NULL;
+    }
+    
+    m_tid = gettid();
+
+    LOG_INFO(LOG_TAG, "%s: thread id %d, thread name %s started", __func__,
+           m_tid, m_name);
+
+    entry->entry_evt->Post();
+    //entry->entry_evt has been free, cannot use it anymore
+
+    int fd = m_pWorkQueue->GetDequeue(); 
+    void* context = m_pWorkQueue;
+
+    reactor_object_t* work_queue_object = m_pReactor->Register(fd, context, work_queue_read_cb, NULL);
+    m_pReactor->Start();
+    m_pReactor->Deregister(work_queue_object);
+
+    // Make sure we dispatch all queued work items before exiting the thread.
+    // This allows a caller to safely tear down by enqueuing a teardown
+    // work item and then joining the thread.
+    size_t count = 0;
+    work_item_t* item = static_cast<work_item_t*>(m_pWorkQueue->TryDequeue());
+    while (item && count <= m_pWorkQueue->Capacity()) {
+        item->func(item->context);
+        sys_free(item);
+        item = static_cast<work_item_t*>(m_pWorkQueue->TryDequeue());
+        ++count;
+    }
+
+    if (count > m_pWorkQueue->Capacity())
+        LOG_DEBUG(LOG_TAG, "%s growing event queue on shutdown.", __func__);
+
+    LOG_WARN(LOG_TAG, "%s: thread id %d, thread name %s exited", __func__,
+           m_tid, m_name);
+    
     return NULL;
-  }
-  thread->tid = gettid();
-
-  LOG_INFO(LOG_TAG, "%s: thread id %d, thread name %s started", __func__,
-           thread->tid, thread->name);
-
-  event_post(entry->entry_evt);
-
-  int fd = fixed_queue_get_dequeue_fd(thread->work_queue);
-  void* context = thread->work_queue;
-
-  reactor_object_t* work_queue_object =
-      reactor_register(thread->reactor, fd, context, work_queue_read_cb, NULL);
-  reactor_start(thread->reactor);
-  reactor_unregister(work_queue_object);
-
-  // Make sure we dispatch all queued work items before exiting the thread.
-  // This allows a caller to safely tear down by enqueuing a teardown
-  // work item and then joining the thread.
-  size_t count = 0;
-  work_item_t* item =
-      static_cast<work_item_t*>(fixed_queue_try_dequeue(thread->work_queue));
-  while (item && count <= fixed_queue_capacity(thread->work_queue)) {
-    item->func(item->context);
-    osi_free(item);
-    item =
-        static_cast<work_item_t*>(fixed_queue_try_dequeue(thread->work_queue));
-    ++count;
-  }
-
-  if (count > fixed_queue_capacity(thread->work_queue))
-    LOG_DEBUG(LOG_TAG, "%s growing event queue on shutdown.", __func__);
-
-  LOG_WARN(LOG_TAG, "%s: thread id %d, thread name %s exited", __func__,
-           thread->tid, thread->name);
-  return NULL;
 }
+
+void* Thread::run_thread(void* arg) {
+    CHECK(arg != NULL);
+    struct entry_arg* entry = static_cast<entry_arg*>(arg);    
+    return entry->thread->Run(arg);
+}
+
 
 static void work_queue_read_cb(void* context) {
   CHECK(context != NULL);
 
-  fixed_queue_t* queue = (fixed_queue_t*)context;
-  work_item_t* item = static_cast<work_item_t*>(fixed_queue_dequeue(queue));
+  FixedQueue* queue = (FixedQueue*)context;
+  work_item_t* item = static_cast<work_item_t*>(queue->Dequeue());
   item->func(item->context);
   sys_free(item);
 }
