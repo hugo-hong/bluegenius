c5c8c910ea36f5ae3d27c45a38bc09aef3f8f983
hugo-hong <hugo.08@163.com>
2017-11-29 17:58:10 +0800

    Update thread.h

--- utils/inc/thread.h
+++ utils/inc/thread.h
@@ -1,4 +1,4 @@
-/*
+/*********************************************************************************
    Bluegenius - Bluetooth host protocol stack for Linux/android/windows...
    Copyright (C) 
    Written 2017 by hugo（yongguang hong） <hugo.08@163.com>
@@ -16,78 +16,48 @@
    ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS,
    COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
    SOFTWARE IS DISCLAIMED.
-*/ 
-#ifndef _THREAD_PROXY_H_
-#define _THREAD_PROXY_H_
+*********************************************************************************/
+#ifndef _THREAD_H_
+#define _THREAD_H_
 
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define THREAD_NAME_MAX       (16)//PR_SET_NAME limit max name length 16 bytes
+#define THREAD_NAME_MAX       		(16)//PR_SET_NAME limit max name length 16 bytes
+#define DEFAULT_WORK_QUEUE_CAPACITY (128)
 
 typedef void (*thread_fn)(void* context);
 
+class Thread {
+public:
+	Thread(const char* name, size_t size = DEFAULT_WORK_QUEUE_CAPACITY);
+	~Thread();
+	
+	void Post(thread_fn func, void* context);
+	void Stop();
+	void Join();
+	bool SetPriority(int priority);
+	bool SetRTPriority(int priority);
+	bool IsSelf();	
+	Reactor* GetReactor() {return m_pReactor;}
+	const char* GetName() {return m_name;}
+	
+protected:
+	void New(const char* name, size_t size);
+	void Free();	
+	void* Run(void* arg);
+	
+	static void* run_thread(void* arg);
+	friend static void work_queue_read_cb(void* context);
+private:
+	std::atomic_bool m_isJoined;
+	pthread_t m_pthread;
+	pid_t m_tid;
+	char m_name[THREAD_NAME_MAX + 1];
+	Reactor* m_pReactor;
+	FixedQueue* m_pWorkQueue;
 
-/*********************************************************************
-	Name:thread_new
-	Para:name,Only THREAD_NAME_MAX bytes from |name| will be assigned
-       to the newly-created thread			
-	Return: Returns a thread object if successfully, NULL otherwise
-	Func:create new thread for given name	
-*********************************************************************/
-thread_t* thread_new(const char* name);
+}
 
-/*********************************************************************
-	Name:thread_free
-	Para:thread, the thread object to free      	
-	Return: none
-	Func: free the given thread.if the thread is running, it is stopped
-        and the calling thread will block until thread terminates.
-*********************************************************************/
-void thread_free(thread_t* thread);
 
-/*********************************************************************
-	Name:thread_join
-	Para:thread, the thread object to be join      	
-	Return: none
-	Func: waits for thead stop.
-*********************************************************************/
-void thread_join(thread_t* thread);
-
-/*********************************************************************
-	Name:thread_post
-	Para:thread, the thread object;func, the function to be excute; 
-        context, func parameter.
-	Return: true on success, otherwise false.
-	Func: call func with argument context. this function typically does 
-        not block unless there are excessive number of functions posted 
-        to thread that have not been dispatched yet.
-*********************************************************************/
-bool thread_post(thread_t* thread, thread_fn func, void* context);
-
-/*********************************************************************
-	Name:thread_stop
-	Para:thread, the thread object to be stop.
-	Return: none.
-	Func: requests thread to stop. 
-        this function is guaranteed to not block.
-*********************************************************************/
-void thread_stop(thread_t* thread);
-
-/*********************************************************************
-	Name:thread_set_priority
-	Para:thread, the thread object;priority, the priority to be set.        
-	Return: true on success, otherwise false.
-	Func: attempts to set the priority of the gien thread.
-*********************************************************************/
-bool thread_set_priority(thread_t* thread, int priority);
-
-/*********************************************************************
-	Name:thread_set_rt_priority
-	Para:thread, the thread object;priority, the priority to be set.        
-	Return: true on success, otherwise false.
-	Func: attempts to set thread to the real-time SCHED_FIFO priority.
-*********************************************************************/
-bool thread_set_rt_priority(thread_t* thread, int priority);
-
-#endif //_THREAD_PROXY_H_
+#endif //_THREAD_H_
