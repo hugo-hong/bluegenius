c1526af12a3d8991f8cc88830cabbd302cd2bc50
hugo-hong <hugo.08@163.com>
2017-12-03 16:43:46 +0800

    Update event.cxx

--- utils/src/event.cxx
+++ utils/src/event.cxx
@@ -1,4 +1,4 @@
-/*
+/*********************************************************************************
    Bluegenius - Bluetooth host protocol stack for Linux/android/windows...
    Copyright (C) 
    Written 2017 by hugo（yongguang hong） <hugo.08@163.com>
@@ -16,7 +16,7 @@
    ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS,
    COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
    SOFTWARE IS DISCLAIMED.
-*/ 
+*********************************************************************************/ 
 
 #define LOG_TAG "event_proxy"
 
@@ -31,76 +31,58 @@
 #define EFD_SEMAPHORE (1 << 0)
 #endif
 
-struct event_t {
-  int fd;
-};
 
-event_t* event_create(unsigned int value) {
-  event_t* event = static_cast<event_t*>sis_malloc(sizeof(event_t));
-  event->fd = eventfd(value, EFD_SEMAPHORE);
-  if (INVALID_FD == event->fd) {
-     LOG_ERROR(LOG_TAG, "%s unable to allocate event: %s", __func__,
-              strerror(errno));
-     sis_free(event);
-     event = NULL;
-  }  
-  return event;
+Event::Event()
+    :m_fd(INVALID_FD)
+{
+    New();
 }
 
-void event_close(event_t *event) {
-  if (NULL == event) return;
-  
-  if (INVALID_FD != event->fd) close(event->fd);
-  sis_free(event);
+Event::~Event() {
+    Free();
 }
 
-void event_wait(event_t *event) {
-  CHECK(event != NULL);
-  CHECK(event->fd != INVALID_FD);
-  
-  eventfd_t value;
-  if (eventfd_read(event->fd, &value) == -1)
-    LOG_ERROR(LOG_TAG, "%s unable to wait on event: %s", __func__,
+void Event::Wait() {
+    eventfd_t value;
+    if (eventfd_read(m_fd, &value) == -1)
+        LOG_ERROR(LOG_TAG, "%s unable to wait on event: %s", __func__,
               strerror(errno));
 }
 
-bool event_try_wait(event_t *event) {
-  CHECK(event != NULL);
-  CHECK(event->fd != INVALID_FD);
-  
-  int flags = fcntl(event->fd, F_GETFL);
-  if (-1 == flags) {
-     LOG_ERROR(LOG_TAG, ""%s unable to get flags for event fd: %s",
+bool Event::TryWait() {
+    int flags = fcntl(m_fd, F_GETFL);
+    if (-1 == flags) {
+        LOG_ERROR(LOG_TAG, ""%s unable to get flags for event fd: %s",
               __func__, strerror(errno));
-    return false;
-  }
-  if (fcntl(event->fd, F_SETFL, flags | O_NONBLOCK) == -1) {
-    LOG_ERROR(LOG_TAG, "%s unable to set O_NONBLOCK for event fd: %s",
+        return false;
+    }
+    if (fcntl(m_fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        LOG_ERROR(LOG_TAG, "%s unable to set O_NONBLOCK for event fd: %s",
               __func__, strerror(errno));
-    return false;
-  }
-  bool ret = true;
-  eventfd_t value;
-  if (eventfd_read(event->fd, &value) == -1) ret = false;
+        return false;
+    }
+    bool ret = true;
+    eventfd_t value;
+    if (eventfd_read(m_fd, &value) == -1) ret = false;
 
-  if (fcntl(event->fd, F_SETFL, flags) == -1)
+    if (fcntl(m_fd, F_SETFL, flags) == -1)
     LOG_ERROR(LOG_TAG, "%s unable to restore flags for event fd: %s",
-              __func__, strerror(errno));
-  return ret;  
+          __func__, strerror(errno));
+    return ret;  
 }
 
-void event_post(event_t *event) {
-  CHECK(event != NULL);
-  CHECK(event->fd != INVALID_FD);
-   
-  if (-1 == eventfd_write(event->fd, 1ULL))
-    LOG_ERROR(LOG_TAG, "%s unable to wait on event: %s", __func__,
-              strerror(errno));
+void Event::Post() {
+    if (-1 == eventfd_write(event->fd, 1ULL))
+        LOG_ERROR(LOG_TAG, "%s unable to wait on event: %s", __func__,
+            strerror(errno));
 }
 
-int event_get_fd(const event_t *event) {
-  CHECK(event != NULL);
-  CHECK(event->fd != INVALID_FD);
-   
-  return event->fd;
+void Event::New() {
+    CHECK(m_fd == INVALID_FD);
+    m_fd = eventfd(value, EFD_SEMAPHORE);
+}
+
+void Event::Free() {
+    if (INVALID_FD != m_fd) close(m_fd);
+    m_fd = INVALID_FD;
 }
